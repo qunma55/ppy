6948528c37e6d1785a2a598e784d8b86b4b6971e
hewei <hewei@xiniaoyun.com>
2020-04-17 18:39:16 +0800

    0.0.2

--- dataBoom.py
+++ dataBoom.py
@@ -12,11 +12,35 @@ command = 0
 prefix = ""
 if PrefixSub != -1:
     command += 1
-    print("==开始生成前缀")
+    print("开始生成前缀")
     PrefixEndSub = Str.find("*")
     prefix = Str[PrefixSub + len('$prefix:{'):PrefixEndSub - 1]
 
-# 查找循环体
-LoopSub = Str.find('$loop:')
+    # 查找循环体
+    LoopSub = Str.find('$loop:')
+    if LoopSub != -1:
+        LoopEndSub = Str.find("*", LoopSub)
+        # 数据量
+        LoopNumber = Str[LoopSub + len('$loop:'): LoopEndSub]
 
-LoopNumber = Str[LoopSub + len('$loop:'):]
+        # 查找循环体里的内容
+        LoopBodySub = Str.find('[', LoopEndSub)
+        LoopBodyEndSub = Str.find(']', LoopEndSub)
+
+        formatData = Str[LoopBodySub: LoopBodyEndSub + 1]
+        List = formatData[2:-2].split(',')
+        for item in List:
+            m = item.find('\"')
+            n = item.find('\":')
+            KeyText = item[m + 1: n]
+            k = item.find('$')
+            j = item.find('*')
+            valueText = item[k + 1: j]
+            if valueText.find('format:') != -1:
+                FormatStr = valueText.split(":")[1]
+                if FormatStr == 'str':
+                    print('字符串')
+                if FormatStr == 'int':
+                    print('数字')
+                if FormatStr == 'uuid':
+                    print('随机数')
