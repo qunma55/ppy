6fe9ce662f82a2a10049f7cbe14c63f9f6636c06
hewei <hewei@xiniaoyun.com>
2020-04-18 17:37:02 +0800

    读取文件

--- dataBoom.py
+++ dataBoom.py
@@ -1,46 +1,51 @@
 # 字符模板
-Str = "$prefix:{pre}*$loop:1000*[{\"value0\": $format:str*,\"value1\": $format:int*,\"value2\": " \
-      "$format:uuid*}]$suffix:{suf}*"
+import random
+import string
+import uuid
 
-Str.strip()
 
-JsonStr = ""
+def boom_str():
+    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+
+
+def boom_int():
+    return random.randint(0, 100000)
+
+
+f = open('template.txt', 'r')
+# 去除所有blank字符
+Str = f.read().replace(" ", "").replace("\n", "")
 # 判断是否有前缀
 PrefixSub = Str.find('$prefix:')
 # 命令起始值
 command = 0
 prefix = ""
 if PrefixSub != -1:
-    command += 1
-    print("开始生成前缀")
     PrefixEndSub = Str.find("*")
     prefix = Str[PrefixSub + len('$prefix:{'):PrefixEndSub - 1]
-
     # 查找循环体
     LoopSub = Str.find('$loop:')
     if LoopSub != -1:
         LoopEndSub = Str.find("*", LoopSub)
         # 数据量
-        LoopNumber = Str[LoopSub + len('$loop:'): LoopEndSub]
-
+        LoopNumber = int(Str[LoopSub + len('$loop:'): LoopEndSub])
         # 查找循环体里的内容
         LoopBodySub = Str.find('[', LoopEndSub)
         LoopBodyEndSub = Str.find(']', LoopEndSub)
 
         formatData = Str[LoopBodySub: LoopBodyEndSub + 1]
-        List = formatData[2:-2].split(',')
-        for item in List:
-            m = item.find('\"')
-            n = item.find('\":')
-            KeyText = item[m + 1: n]
-            k = item.find('$')
-            j = item.find('*')
-            valueText = item[k + 1: j]
-            if valueText.find('format:') != -1:
-                FormatStr = valueText.split(":")[1]
-                if FormatStr == 'str':
-                    print('字符串')
-                if FormatStr == 'int':
-                    print('数字')
-                if FormatStr == 'uuid':
-                    print('随机数')
+        formatBody = formatData[2:-2]
+        LoopBody = ''
+        for x in list(range(LoopNumber)):
+            random.seed(x)
+            itemFormatBody = formatBody.replace("$format:str*", "\"" + boom_str() + "\"") \
+                .replace("$format:int*", str(boom_int())) \
+                .replace("$format:uuid*", "\"" + str(uuid.uuid4()) + "\"")
+            itemFormatBody = "{" + itemFormatBody + "},"
+            LoopBody += itemFormatBody
+        LoopBody = "[" + LoopBody[0:-1] + "]"
+        SufStartSub = Str.find("$suffix:{")
+        SufEndSub = Str.rfind("*")
+        SufStr = Str[SufStartSub + len('$prefix:{'):SufEndSub - 1]
+        BoomStr = prefix + LoopBody + SufStr
+        print(BoomStr)
